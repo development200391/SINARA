29a8b0a32284292c90f4edc7fd482ce6fe0b58b2
Ramdan Nur <ramdan.nur@bsi.co.id>
2026-05-12 09:10:35 +0700

    Add project files.

---
+++ SINARA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SINARA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
