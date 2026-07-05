288acd4e8b4c07f6d9ff7a4566bb520371db27c5
Ramdan Nur <development200391@gmail.com>
2026-07-05 11:19:42 +0700

    remove porject sinara, beause its no need

--- SINARA/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SINARA.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
